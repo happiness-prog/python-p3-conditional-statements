eddae54b05eb311b7ca3f0735e43afb5f42841b8
Happiness Maina <mainahappiness361@gmail.com>
2025-02-17 10:50:42 +0300

    add file

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,58 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
+    if (username == "admin" or username == "ADMIN") and password == "12345":
+        access = "Access granted"
+    else:
+        access = "Access denied"
+    
+    return access
+
+
+
     # your code here
     pass
 
 def hows_the_weather(temperature):
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif 40 <= temperature <= 65:
+        return "It's a little chilly out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
+
     # your code here
     pass
+def fizzbuzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
+    else:
+        return number
 
-def fizzbuzz(num):
+
     # your code here
     pass
 
 def calculator(operation, num1, num2):
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        if num2 == 0:
+            return "Cannot divide by zero!"
+        return num1 / num2
+    else:
+        return None  # Return None instead of the error message
+
+
     # your code here
     pass
